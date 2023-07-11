69f90838b7a0feeb808a3f4540fd4031bb7f2341
Yotam Nachum <me@yotam.net>
2023-07-11 21:20:54 +0300

    Add basic battle renderer

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
+import Battle from "./renderers/battle-scene";
 
 // Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
+let app = new PIXI.Application({
+  resizeTo: window,
+});
 document.body.appendChild(app.view);
 
-// Create the sprite and add it to the stage
-let sprite = PIXI.Sprite.from('/assets/sample.png');
-app.stage.addChild(sprite);
-
-// Add a ticker callback to move the sprite back and forth
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-  elapsed += delta;
-  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-});
+const battle = new Battle();
+battle.create(app.stage);
